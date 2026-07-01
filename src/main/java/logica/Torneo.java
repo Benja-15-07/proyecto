8ef286c43d92bda56c1c2bc4b8c29f96fbd7f26a
Benja <benjaminrrivassepul@gmail.com>
2026-07-01 19:32:51 -0400

    feat: crea estructura base de Torneo con atributos y listas

--- src/main/java/logica/Torneo.java
+++ src/main/java/logica/Torneo.java
@@ -1,5 +1,29 @@
 package logica;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Torneo {
+    private String nombre;
+    private String disciplina;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private FormatoStrategy formato;
+    private CriterioStrategy criterio;
 
+    private ArrayList<Participante> participantes;
+    private ArrayList<Enfrentamiento> enfrentamientos;
+    private Clasificacion clasificacion;
+
+
+    public Torneo(String nombre, String disciplina, LocalDate fechaInicio, LocalDate fechaFin, FormatoStrategy formato, CriterioStrategy criterio) {
+        this.nombre = nombre;
+        this.disciplina = disciplina;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.formato = formato;
+        this.criterio = criterio;
+        this.participantes = new ArrayList<>();
+        this.enfrentamientos = new ArrayList<>();
+        this.clasificacion = new Clasificacion();
+    }
 }
