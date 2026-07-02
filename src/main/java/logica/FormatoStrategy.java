bafa03a0524081369cd0e92f62d206adb12f4074
Benja <benjaminrrivassepul@gmail.com>
2026-07-01 22:03:03 -0400

    feat: crea interfaz FormatoStrategy con metodo generarEnfrentamientos

---
+++ src/main/java/logica/FormatoStrategy.java
@@ -0,0 +1,7 @@
+package logica;
+
+import java.util.ArrayList;
+
+public interface FormatoStrategy {
+    public ArrayList<Enfrentamiento> generarEnfrentamientos(ArrayList<Participante> participantes);
+}
