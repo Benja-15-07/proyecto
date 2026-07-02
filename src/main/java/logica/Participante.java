51db8a1a13665a95c1b3f123eedcd2d857d0a462
Benja <benjaminrrivassepul@gmail.com>
2026-07-01 21:18:29 -0400

    feat: agrega estructura base de Participante

--- src/main/java/logica/Participante.java
+++ src/main/java/logica/Participante.java
@@ -1,4 +1,35 @@
 package logica;
 
-public class Participante {
+public abstract class Participante {
+    private static int contadorID = 1;
+
+    private int id;
+    private String nombre;
+    private String contacto;
+
+    private Estadistica estadistica;
+
+    public Participante(String nombre, String contacto) {
+        this.id = contadorID++;
+        this.nombre = nombre;
+        this.contacto = contacto;
+
+        this.estadistica = new Estadistica();
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public Estadistica getEstadistica() {
+        return estadistica;
+    }
 }
