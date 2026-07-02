391fdc3a881a820103723c22a7ffbb42851850a1
Benja <benjaminrrivassepul@gmail.com>
2026-07-01 21:52:30 -0400

    feat: implementa estructura base de Calendario

--- src/main/java/logica/Calendario.java
+++ src/main/java/logica/Calendario.java
@@ -1,4 +1,15 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Calendario {
+    private ArrayList<Enfrentamiento> calendario;
+
+    public Calendario() {
+        this.calendario = new ArrayList<>();
+    }
+    
+    public ArrayList<Enfrentamiento> getCalendario() {
+        return new ArrayList<>(calendario);
+    }
 }
