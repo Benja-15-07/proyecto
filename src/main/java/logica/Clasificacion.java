45c9760bb7b2b4b5b0de8599a101247028eaf88c
Benja <benjaminrrivassepul@gmail.com>
2026-07-01 21:49:01 -0400

    feat: implementa estructura base de Clasificacion

--- src/main/java/logica/Clasificacion.java
+++ src/main/java/logica/Clasificacion.java
@@ -1,4 +1,15 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Clasificacion {
+    private ArrayList<Participante> clasificacion;
+
+    public Clasificacion(){
+        this.clasificacion = new ArrayList<>();
+    }
+
+    public ArrayList<Participante> getClasificacion() {
+        return  new ArrayList<>(clasificacion);
+    }
 }
