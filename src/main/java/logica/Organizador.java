27112b144801fa51c55c307084f830a06f8f6266
Benja <benjaminrrivassepul@gmail.com>
2026-07-01 21:24:29 -0400

    feat: permite crear torneos

--- src/main/java/logica/Organizador.java
+++ src/main/java/logica/Organizador.java
@@ -1,5 +1,7 @@
 package logica;
 
+import java.time.LocalDate;
+
 public class Organizador {
     private String nombre;
     private String contacto;
@@ -7,5 +9,9 @@ public class Organizador {
     public Organizador(String nombre, String contacto) {
         this.nombre = nombre;
         this.contacto = contacto;
+    }
+
+    public Torneo crearTorneo(String nombre, String disciplina, LocalDate fechaInicio, LocalDate fechaFin, FormatoStrategy formato, CriterioStrategy criterio){
+        return new Torneo(nombre, disciplina, fechaInicio, fechaFin, formato, criterio);
     }
 }
