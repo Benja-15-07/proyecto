206b8b6d651d4cb50d7f87978fe69c94bf1ecb39
Benja <benjaminrrivassepul@gmail.com>
2026-07-01 21:15:55 -0400

    feat: agrega estructura base para guardar puntuaciones

--- src/main/java/logica/Resultado.java
+++ src/main/java/logica/Resultado.java
@@ -1,4 +1,19 @@
 package logica;
 
 public class Resultado {
+    private int puntuacion1;
+    private int puntuacion2;
+
+    public Resultado(int puntuacion1, int puntuacion2) {
+        this.puntuacion1 = puntuacion1;
+        this.puntuacion2 = puntuacion2;
+    }
+
+    public int getPuntuacion1() {
+        return puntuacion1;
+    }
+
+    public int getPuntuacion2() {
+        return puntuacion2;
+    }
 }
