6a0635ccd2215a19c74b96bd017cf57c9c700cf0
Benja <benjaminrrivassepul@gmail.com>
2026-07-01 21:43:39 -0400

    feat: implementa estructura base de Enfrentamiento

--- src/main/java/logica/Enfrentamiento.java
+++ src/main/java/logica/Enfrentamiento.java
@@ -1,4 +1,30 @@
 package logica;
 
 public class Enfrentamiento {
+    private Participante participante1;
+    private Participante participante2;
+    private Resultado    resultado;
+    private Participante ganador;
+
+    public Enfrentamiento(Participante participante1, Participante participante2) {
+        this.participante1 = participante1;
+        this.participante2 = participante2;
+        this.resultado = null;
+    }
+
+    public Participante getParticipante1() {
+        return participante1;
+    }
+
+    public Participante getParticipante2() {
+        return participante2;
+    }
+
+    public Resultado getResultado() {
+        return resultado;
+    }
+
+    public Participante getGanador() {
+        return ganador;
+    }
 }
