8afd1150d1ab4a9de36959beecfa584b249e5b61
Benja <benjaminrrivassepul@gmail.com>
2026-07-01 21:21:56 -0400

    feat: agrega estructura base de Organizador

--- src/main/java/logica/Organizador.java
+++ src/main/java/logica/Organizador.java
@@ -1,4 +1,11 @@
 package logica;
 
 public class Organizador {
+    private String nombre;
+    private String contacto;
+
+    public Organizador(String nombre, String contacto) {
+        this.nombre = nombre;
+        this.contacto = contacto;
+    }
 }
