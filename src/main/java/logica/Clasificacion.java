2c5907268d0690d6dec71f722325f002f88ac2d4
Benja <benjaminrrivassepul@gmail.com>
2026-07-01 21:50:24 -0400

    feat: implementa metodo para actualizar y ordenar la clasificacion

--- src/main/java/logica/Clasificacion.java
+++ src/main/java/logica/Clasificacion.java
@@ -1,12 +1,20 @@
 package logica;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Clasificacion {
     private ArrayList<Participante> clasificacion;
 
     public Clasificacion(){
         this.clasificacion = new ArrayList<>();
+    }
+
+    public void actualizar(ArrayList<Participante> participantes){
+        clasificacion.clear();
+        clasificacion.addAll(participantes);
+        clasificacion.sort(Comparator.comparingDouble(
+                (Participante p) -> p.getEstadistica().getPorcentajeVictorias()).reversed());
     }
 
     public ArrayList<Participante> getClasificacion() {
