486c651ce17d5a7ccc4f3678526d7334e447878b
Benja <benjaminrrivassepul@gmail.com>
2026-07-08 16:14:28 -0400

    feat: implementa estructura base del controlador

--- src/main/java/ControladorTorneo.java
+++ src/main/java/ControladorTorneo.java
@@ -1,7 +1,22 @@
-package controlador;
-    import logica.*;
-    import visual.*;
+import logica.*;
+import visual.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ControladorTorneo {
-    
+    private Organizador organizador;
+    private Ventana ventana;
+    private Map<String, Enfrentamiento> enfrentamientosMap;
+
+    public ControladorTorneo(Ventana ventana){
+        this.ventana = ventana;
+        this.organizador = new Organizador("Organizador","");
+        this.enfrentamientosMap = new HashMap<>();
+        eventos();
+    }
+
+    private void eventos(){
+        
+    }
 }
